064096353c54d6e8b5d7b316ef45ae5f578bd956
kamden-rasmussen <kamdenr@gmail.com>
2023-02-15 21:37:50 -0700

    cleaning up db location and pulling.

---
+++ cmd/db.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func connectToDatabase() (*sql.DB, error) {
+	// connect to a database
+	config := mysql.Config{
+        User:      os.Getenv("DBACCESSUSERNAME"),
+		Passwd:    os.Getenv("DBACCESSPASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("DBHOST"),
+		DBName:    os.Getenv("DBDATABASENAME"),
+    }
+	db, err := sql.Open("mysql", config.FormatDSN())
+	pingErr := db.Ping()
+    if pingErr != nil {
+        log.Fatal(pingErr)
+    }
+	return db, err
+}
+
+func getFunTable(db *sql.DB) {
+	results, err := db.Query("SELECT * FROM fun")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for results.Next() {
+		var id int
+		var name string
+		var color string
+		err = results.Scan(&id, &name, &color)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		// log.Println(id, name, color)
+	}
+
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"database/sql"
-
 	"github.com/Kamden-Rasmussen/PickupandGO/pkg/handlers"
 	"github.com/Kamden-Rasmussen/PickupandGO/src/cron"
 	"github.com/Kamden-Rasmussen/PickupandGO/src/data"
-	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
 func main(){
 	_ = context.Background()
 
+	// create log file
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+
+
 	// load env variables
-	err := godotenv.Load(".env")
+	err = godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Failed to load env. Err: %s", err)
 	}
@@ -30,7 +34,7 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Println("Connected to Database")
+		log.Println("Connected to database")
 	}
 	defer db.Close()
 	getFunTable(db)
@@ -38,52 +42,16 @@ func main(){
 	// start cron jobs
 	startCron()
 
-	fmt.Println("Starting Server on Port 8018")
+	// start server
+	log.Println("Starting Server on Port 8018")
 
 	router := handlers.InitializeHandlers()
 	log.Fatal(http.ListenAndServe(":8018", router))
 }
 
-func connectToDatabase() (*sql.DB, error) {
-	// connect to a database
-	fmt.Println(os.Getenv("DBACCESSUSERNAME"))
-	config := mysql.Config{
-        User:      os.Getenv("DBACCESSUSERNAME"),
-		Passwd:    os.Getenv("DBACCESSPASSWORD"),
-		Net:       "tcp",
-		Addr:      os.Getenv("DBHOST"),
-		DBName:    os.Getenv("DBDATABASENAME"),
-    }
-	db, err := sql.Open("mysql", config.FormatDSN())
-	pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
-	return db, err
-
-}
-
 func startCron() {
 	cron := cron.NewCron()
 	cron.AddFunc("@every 60s", data.PrintHealth)
-	cron.AddFunc("@every 12h", data.GetData)
+	// cron.AddFunc("@every 12h", data.GetData)
 	cron.Start()
-}
-
-func getFunTable(db *sql.DB) {
-	results, err := db.Query("SELECT * FROM fun")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	for results.Next() {
-		var id int
-		var name string
-		var color string
-		err = results.Scan(&id, &name, &color)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		fmt.Println(id, name, color)
-	}
-
 }
