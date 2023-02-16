911838fa64f0b871f630ff4cb27c354f6a280552
kamden-rasmussen <kamdenr@gmail.com>
2023-02-15 21:36:23 -0700

    adding endpoint to init get data

--- pkg/handlers/handelHealthcheck.go
+++ pkg/handlers/handelHealthcheck.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/Kamden-Rasmussen/PickupandGO/src/data"
 )
 
 func GetHealthCheck() func(w http.ResponseWriter, r *http.Request) {
@@ -9,5 +11,12 @@ func GetHealthCheck() func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`Server is up and running`))
+	}
+}
+
+func GetDataEndpoint() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		data.GetData()
 	}
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -11,5 +11,7 @@ func InitializeHandlers() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/healthcheck", GetHealthCheck()).Methods("GET")
 
+	router.HandleFunc("/api/v1/getall", GetDataEndpoint()).Methods("GET")
+
 	return router
 }
