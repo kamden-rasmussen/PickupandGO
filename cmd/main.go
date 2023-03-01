7fc528649d8323cd03c9724b84cfb5b437f85649
kamden-rasmussen <kamdenr@gmail.com>
2023-02-28 22:47:15 -0700

    cleaning up main. moving some things around

--- cmd/main.go
+++ cmd/main.go
@@ -7,10 +7,10 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Kamden-Rasmussen/PickupandGO/pkg/cron"
+	"github.com/Kamden-Rasmussen/PickupandGO/pkg/data"
 	"github.com/Kamden-Rasmussen/PickupandGO/pkg/handlers"
-	"github.com/Kamden-Rasmussen/PickupandGO/src/cron"
-	"github.com/Kamden-Rasmussen/PickupandGO/src/data"
-	"github.com/go-sql-driver/mysql"
+	"github.com/Kamden-Rasmussen/PickupandGO/pkg/mydatabase"
 	"github.com/joho/godotenv"
 )
 
@@ -34,7 +34,7 @@ func main(){
 	}
 
 	// connect to a database
-	db, err = Init()
+	db, err = mydatabase.Init()
 	if err != nil {
 		log.Fatal(err)
 	} else {
@@ -59,7 +59,7 @@ func main(){
 func startCron(db *sql.DB) {
 	cron := cron.NewCron()
 	log.Println("Starting Cron Jobs")
-	cron.AddFunc("@every 30m", allHealthChecks)
+	cron.AddFunc("@every 1m", allHealthChecks)
 	// cron.AddFunc("@every 12h", data.GetData)
 	// cron.AddFunc("0 0 1 * * *", data.PrintLetsGo)
 	cron.AddFunc("0 0 08 * * *", data.PrintLetsGo)
@@ -69,102 +69,11 @@ func startCron(db *sql.DB) {
 
 func allHealthChecks() {
 	// log.Println("Application is running and healthy")
-	err := dbHealthCheck(db)
+	err := mydatabase.DbHealthCheck(db)
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		log.Println("Database and connection are healthy")
 	}
 	
-}
-
-func Init() (*sql.DB, error) {
-	// connect to a database
-	config := mysql.Config{
-        User:      os.Getenv("DBACCESSUSERNAME"),
-		Passwd:    os.Getenv("DBACCESSPASSWORD"),
-		Net:       "tcp",
-		Addr:      os.Getenv("DBHOST"),
-		DBName:    os.Getenv("DBDATABASENAME"),
-    }
-	db, err := sql.Open("mysql", config.FormatDSN())
-	pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
-	return db, err
-}
-
-func SetUpTables(db *sql.DB) {
-
-	// remove security for now
-	_, err := db.Exec("SET SESSION sql_require_primary_key = 0;")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	query := os.Getenv("CREATE_USERS_TABLE")
-	// create users table
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	
-	// create airports table
-	query = os.Getenv("CREATE_AIRPORTS_TABLE")
-
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// create destinations table
-	query = os.Getenv("CREATE_DESTINATIONS_TABLE")
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// create companies table
-	query = os.Getenv("CREATE_COMPANIES_TABLE")
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// create airlines table
-	query = os.Getenv("CREATE_AIRLINES_TABLE")
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// create flights table
-	query = os.Getenv("CREATE_FLIGHTS_TABLE")
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-}
-
-func GetFunTable(db *sql.DB) {
-	results, err := db.Query("SELECT * FROM fun")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	for results.Next() {
-		var id int
-		var name string
-		var color string
-		err = results.Scan(&id, &name, &color)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		// log.Println(id, name, color)
-	}
-
-}
-
-func dbHealthCheck(db *sql.DB) error {
-	return db.Ping()
 }
