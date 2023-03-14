4e65490c27e1d15c5d93d230ec7f799987cdda48
kamden-rasmussen <kamdenr@gmail.com>
2023-03-14 10:34:58 -0600

    setting up retrieving user

--- pkg/data/users.go
+++ pkg/data/users.go
@@ -3,6 +3,8 @@ package data
 import (
 	"database/sql"
 	"log"
+
+	"github.com/Kamden-Rasmussen/PickupandGO/pkg/mydatabase"
 )
 
 type User struct {
@@ -14,8 +16,8 @@ type User struct {
 	Currency  string
 }
 
-func GetAllUsers(db *sql.DB) []User {
-	results, err := db.Query("SELECT * FROM users")
+func GetAllUsers() []User {
+	results, err := mydatabase.MyDB.Query("SELECT * FROM users")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -34,4 +36,33 @@ func GetAllUsers(db *sql.DB) []User {
 		users = append(users, User{id, firstname, lastname, email, home, currency})
 	}
 	return users
+}
+
+func AddUser(user User) {
+	_, err := mydatabase.MyDB.Exec("INSERT INTO users (firstname, lastname, email, home, currency) values( ?, ?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Home, user.Currency)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func GetOneUser(db *sql.DB, id int) User {
+	results, err := mydatabase.MyDB.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	var user User
+	for results.Next() {
+		var id int
+		var firstname string
+		var lastname string
+		var email string
+		var home int
+		var currency string
+		err = results.Scan(&id, &firstname, &lastname, &email, &home, &currency)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		user = User{id, firstname, lastname, email, home, currency}
+	}
+	return user
 }
