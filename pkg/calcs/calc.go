8f85912bd40fc8def7879c251f927101d8f8cb8e
kamden-rasmussen <kamdenr@gmail.com>
2023-03-20 13:15:47 -0600

    continuing calc work

--- pkg/calcs/calc.go
+++ pkg/calcs/calc.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/Kamden-Rasmussen/PickupandGO/pkg/data"
+	"github.com/Kamden-Rasmussen/PickupandGO/pkg/mydatabase"
 )
 
 
@@ -24,6 +25,38 @@ func BeginCalc(){
 	users := data.GetAllUsers()
 	log.Println(users)
 
+	// Get data for each user
+	for _, user := range users {
+
+		// get data per destination 
+		destinations := data.GetDestinationsForUser(user.Id)
+		for _, destination := range destinations {
+			log.Println(destination)
+			// get price for each destination
+			oneWeekPrice := GetPriceByDate(oneWeek, destination)
+			twoWeeksPrice := GetPriceByDate(twoWeeks, destination)
+			oneMonthPrice := GetPriceByDate(oneMonth, destination)
+			threeMonthsPrice := GetPriceByDate(threeMonths, destination)
+			sixMonthsPrice := GetPriceByDate(sixMonths, destination)
+			oneYearPrice := GetPriceByDate(oneYear, destination)
+			log.Println(oneWeekPrice, twoWeeksPrice, oneMonthPrice, threeMonthsPrice, sixMonthsPrice, oneYearPrice)
+			// TODO: start calcing
+		}
+	}
 }
 
-
+func GetPriceByDate(date string, destination int) float32 {
+	// get price for each destination
+	rows, err := mydatabase.MyDB.Query("SELECT * FROM flights WHERE departure_date = ? AND arrival_location = ? order by price asc limit 1", date, destination)
+	if err != nil {
+		panic(err.Error())
+	}
+	var price float32
+	for rows.Next() {
+		err = rows.Scan(&price)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	return price
+}
--- pkg/calcs/dates.go
+++ pkg/calcs/dates.go
@@ -2,39 +2,40 @@ package calcs
 
 import "time"
 
+// need to offset by 7 dats to get the correct date
 
 func GetOneWeekAgo() string {
 	t := time.Now()
-	OneWeekDate := t.AddDate(0, 0, 7).Format("2006-01-02")
+	OneWeekDate := t.AddDate(0, 0, 0).Format("2006-01-02")
 	return OneWeekDate
 }
 
 func GetTwoWeeksAgo() string {
 	t := time.Now()
-	TwoWeeksDate := t.AddDate(0, 0, 14).Format("2006-01-02")
+	TwoWeeksDate := t.AddDate(0, 0, -7).Format("2006-01-02")
 	return TwoWeeksDate
 }
 
 func GetOneMonthAgo() string {
 	t := time.Now()
-	OneMonthDate := t.AddDate(0, 0, 30).Format("2006-01-02")
+	OneMonthDate := t.AddDate(0, 0, -23).Format("2006-01-02")
 	return OneMonthDate
 }
 
 func GetThreeMonthsAgo() string {
 	t := time.Now()
-	ThreeMonthsDate := t.AddDate(0, 0, 90).Format("2006-01-02")
+	ThreeMonthsDate := t.AddDate(0, 0, -83).Format("2006-01-02")
 	return ThreeMonthsDate
 }
 
 func GetSixMonthsAgo() string {
 	t := time.Now()
-	SixMonthsDate := t.AddDate(0, 0, 180).Format("2006-01-02")
+	SixMonthsDate := t.AddDate(0, 0, -173).Format("2006-01-02")
 	return SixMonthsDate
 }
 
 func GetOneYearAgo() string {
 	t := time.Now()
-	OneYearDate := t.AddDate(0, 0, 365).Format("2006-01-02")
+	OneYearDate := t.AddDate(0, 0, -358).Format("2006-01-02")
 	return OneYearDate
 }
