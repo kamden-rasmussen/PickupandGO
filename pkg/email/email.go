d44aa3e4453563cb4675a2323d10757ba4621f34
kamden-rasmussen <kamdenr@gmail.com>
2023-03-27 14:36:17 -0600

    completed work on generating emails

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -2,49 +2,47 @@ package email
 
 import (
 	"log"
-	"net/smtp"
 	"os"
 	"strconv"
 
+	"github.com/Kamden-Rasmussen/PickupandGO/pkg/data"
 	"github.com/joho/godotenv"
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-func SendEmail(email string, body string) {
-	
+func SendEmail(user data.User, body string, html string) int{
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Failed to load env. Err: %s", err)
 	}
 
 	senderEmail := os.Getenv("SENDING_EMAIL")
-	senderPassword := os.Getenv("SENDING_EMAIL_PASSWORD")
-	
-	from := senderEmail
-	pass := senderPassword
-	to := email
 
-	auth := smtp.PlainAuth("", from, pass, "smtp.gmail.com")
+	// who
+	from := mail.NewEmail("Pick Up and GO", senderEmail)
+	subject := "Pickup and Go Alert - Price Drop for destination: " + "!" // TODO: add destination
+	to := mail.NewEmail(user.FirstName + " " + user.LastName, user.Email)
 
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: " + //SUBJECT HERE +
-		body
+	// content
+	plainTextContent := body
+	htmlContent := html
 
-	err = smtp.SendMail("smtp.gmail.com:587",
-		auth,
-		// auth := smtp.PlainAuth("", "john.doe@gmail.com", "extremely_secret_pass", "smtp.gmail.com")
-		from, 
-		[]string{to}, 
-		[]byte(msg))
+	// setup
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 
+	// SHIPIT
+	response, err := client.Send(message)
+	log.Printf("Sent Email with response code:" + strconv.Itoa(response.StatusCode))
 	if err != nil {
-		log.Printf("smtp error: %s", err)
-		return
+		log.Println(err)
+	} else {
+		log.Println(response.StatusCode)
+		log.Println(response.Body)
+		log.Println(response.Headers)
 	}
-
-	log.Println("Email Sent Successfully to " + email + "")
+	return response.StatusCode
 }
 
 func SendTestEmail(email string, body string) string{
@@ -60,9 +58,14 @@ func SendTestEmail(email string, body string) string{
 	subject := "Test Email from Pickup and Go"
 	to := mail.NewEmail("TestUser", email)
 
+	testUser := data.User{
+		FirstName: "Test",
+		LastName: "User",
+		Email: email,
+	}
+
 	// content
-	plainTextContent := "and easy to do anywhere, even with Go" + body
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	plainTextContent, htmlContent := SetupEmail(testUser, "Party Place", "2020-01-01", "2020-01-01", 100.00, 100.00, 100.00, 100.00, 100.00, 100.00)
 
 	// setup
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
@@ -79,4 +82,100 @@ func SendTestEmail(email string, body string) string{
 		log.Println(response.Headers)
 	}
 	return "Email Sent Successfully to " + email + ""
+}
+
+func SetupEmail(user data.User, location string, departureDate string, returnDate string, currentPrice float64, sevenDay float64, fourteenDay float64, thirtyDay float64, nintyDay float64, oneYear float64) (body string, html string) {
+
+	// body
+	body = "Hello " + user.FirstName + ",\n\n"
+	body += "A Flight you are tracking to " + location + " has had a signifigant price drop!\n\n"
+	body += "The flight details are as follows \n"
+	body += "Departure Date: " + departureDate + "\n"
+	body += "Return Date: " + returnDate + "\n"
+	body += "Current Price: " + strconv.FormatFloat(float64(currentPrice), 'f', 2, 64) + "\n\n"
+	body += "Price History \n"
+	body += "Todays Price: " + strconv.FormatFloat(float64(currentPrice), 'f', 2, 64) + "\n"
+	body += "7 Days: " + strconv.FormatFloat(float64(sevenDay), 'f', 2, 64) + "\n"
+	body += "14 Days: " + strconv.FormatFloat(float64(fourteenDay), 'f', 2, 64) + "\n"
+	body += "30 Days: " + strconv.FormatFloat(float64(thirtyDay), 'f', 2, 64) + "\n"
+	body += "90 Days: " + strconv.FormatFloat(float64(nintyDay), 'f', 2, 64) + "\n"
+	body += "1 Year: " + strconv.FormatFloat(float64(oneYear), 'f', 2, 64) + "\n"
+
+
+	color := "green"
+	// html
+	html = "<h1>Hello " + user.FirstName + ",</h1>"
+	html += "<p>A Flight you are tracking to " + location + " has had a signifigant price drop!</p>"
+	html += "<p> Details of the flight are as follows </p>"
+	html += "<p>Departure Date: " + departureDate + "</p>"
+	html += "<p>Return Date: " + returnDate + "</p>"
+	html += "<p>Current Price: " + "$" + strconv.FormatFloat(float64(currentPrice), 'f', 2, 64) + "</p>"
+	html += "<p>The flight History is as follows :</p>"
+	// table of price checks
+	html += "<table style=\"width:100%\">"
+	html += "<tr>"
+	html += "<th>Price Check</th>"
+	html += "<th>Price</th>"
+	html += "</tr>"
+	html += "<tr>"
+	html += "<td style=\"text-align: center\">Todays Price</td>"
+	if currentPrice < sevenDay {
+		color = "red"
+	} else {
+		color = "green"
+	}
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(currentPrice), 'f', 2, 64) + "</td>"
+	html += "</tr>"
+	html += "<tr>"
+	html += "<td style=\"text-align: center\">7 Days</td>"
+	if currentPrice < sevenDay {
+		color = "red"
+	} else {
+		color = "green"
+	}
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(sevenDay), 'f', 2, 64) + "</td>"
+	html += "</tr>"
+	html += "<tr>"
+	html += "<td style=\"text-align: center\">14 Days</td>"
+	if currentPrice < fourteenDay {
+		color = "red"
+	} else {
+		color = "green"
+	}
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(fourteenDay), 'f', 2, 64) + "</td>"
+	html += "</tr>"
+	html += "<tr>"
+	html += "<td style=\"text-align: center\">30 Days</td>"
+	if currentPrice < thirtyDay {
+		color = "red"
+	} else {
+		color = "green"
+	}
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(thirtyDay), 'f', 2, 64) + "</td>"
+	html += "</tr>"
+	html += "<tr>"
+	html += "<td style=\"text-align: center\">90 Days</td>"
+	if currentPrice < nintyDay {
+		color = "red"
+	} else {
+		color = "green"
+	}
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(nintyDay), 'f', 2, 64) + "</td>"
+	html += "</tr>"
+	html += "<tr>"
+	html += "<td style=\"text-align: center\">1 Year</td>"
+	if currentPrice < oneYear {
+		color = "red"
+	} else {
+		color = "green"
+	}
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(oneYear), 'f', 2, 64) + "</td>"
+	html += "</tr>"
+	html += "</table>"
+
+	// html += "<p>Click <a href=\"https://www.google.com/flights?hl=en#flt=/m/0h8j./m/0h8j." + departureDate + "*/m/0h8j." + returnDate + ";c:USD;e:1;sd:1;t:f\">here</a> to view the flight on Google Flights</p>"
+
+
+
+	return body, html
 }
