68921183a16970d40f001ec5e64ed966693e0f6a
kamden-rasmussen <kamdenr@gmail.com>
2023-03-27 15:50:30 -0600

    working on emails. getting there.

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -11,7 +11,7 @@ import (
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-func SendEmail(user data.User, body string, html string) int{
+func SendEmail(user data.User, prices []float64, destination int) int{
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Failed to load env. Err: %s", err)
@@ -19,14 +19,15 @@ func SendEmail(user data.User, body string, html string) int{
 
 	senderEmail := os.Getenv("SENDING_EMAIL")
 
+	destString := data.GetAirportById(destination)
+
 	// who
 	from := mail.NewEmail("Pick Up and GO", senderEmail)
 	subject := "Pickup and Go Alert - Price Drop for destination: " + "!" // TODO: add destination
 	to := mail.NewEmail(user.FirstName + " " + user.LastName, user.Email)
 
 	// content
-	plainTextContent := body
-	htmlContent := html
+	plainTextContent, htmlContent := SetupEmail(user, destString, data.GetDepartureDate(), data.GetReturnDate(), prices)
 
 	// setup
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
@@ -64,8 +65,10 @@ func SendTestEmail(email string, body string) string{
 		Email: email,
 	}
 
+	prices := []float64{100.00, 200.00, 300.00, 50.00, 400.00, 500.00}
+
 	// content
-	plainTextContent, htmlContent := SetupEmail(testUser, "Party Place", "2020-01-01", "2020-01-01", 100.00, 100.00, 100.00, 100.00, 100.00, 100.00)
+	plainTextContent, htmlContent := SetupEmail(testUser, "Party Place", "2020-01-01", "2020-01-01", prices)
 
 	// setup
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
@@ -84,7 +87,14 @@ func SendTestEmail(email string, body string) string{
 	return "Email Sent Successfully to " + email + ""
 }
 
-func SetupEmail(user data.User, location string, departureDate string, returnDate string, currentPrice float64, sevenDay float64, fourteenDay float64, thirtyDay float64, nintyDay float64, oneYear float64) (body string, html string) {
+func SetupEmail(user data.User, location string, departureDate string, returnDate string, prices []float64) (body string, html string) {
+
+	currentPrice := prices[0]
+	sevenDay := prices[1]
+	fourteenDay := prices[2]
+	thirtyDay := prices[3]
+	ninetyDay := prices[4]
+	oneYear := prices[5]
 
 	// body
 	body = "Hello " + user.FirstName + ",\n\n"
@@ -98,7 +108,7 @@ func SetupEmail(user data.User, location string, departureDate string, returnDat
 	body += "7 Days: " + strconv.FormatFloat(float64(sevenDay), 'f', 2, 64) + "\n"
 	body += "14 Days: " + strconv.FormatFloat(float64(fourteenDay), 'f', 2, 64) + "\n"
 	body += "30 Days: " + strconv.FormatFloat(float64(thirtyDay), 'f', 2, 64) + "\n"
-	body += "90 Days: " + strconv.FormatFloat(float64(nintyDay), 'f', 2, 64) + "\n"
+	body += "90 Days: " + strconv.FormatFloat(float64(ninetyDay), 'f', 2, 64) + "\n"
 	body += "1 Year: " + strconv.FormatFloat(float64(oneYear), 'f', 2, 64) + "\n"
 
 
@@ -119,11 +129,6 @@ func SetupEmail(user data.User, location string, departureDate string, returnDat
 	html += "</tr>"
 	html += "<tr>"
 	html += "<td style=\"text-align: center\">Todays Price</td>"
-	if currentPrice < sevenDay {
-		color = "red"
-	} else {
-		color = "green"
-	}
 	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(currentPrice), 'f', 2, 64) + "</td>"
 	html += "</tr>"
 	html += "<tr>"
@@ -155,12 +160,12 @@ func SetupEmail(user data.User, location string, departureDate string, returnDat
 	html += "</tr>"
 	html += "<tr>"
 	html += "<td style=\"text-align: center\">90 Days</td>"
-	if currentPrice < nintyDay {
+	if currentPrice < ninetyDay {
 		color = "red"
 	} else {
 		color = "green"
 	}
-	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(nintyDay), 'f', 2, 64) + "</td>"
+	html += "<td style=\"text-align: center\" style=\"color: " + color + "\">" + "$" + strconv.FormatFloat(float64(ninetyDay), 'f', 2, 64) + "</td>"
 	html += "</tr>"
 	html += "<tr>"
 	html += "<td style=\"text-align: center\">1 Year</td>"
